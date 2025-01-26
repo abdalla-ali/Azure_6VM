5970a45d25247940a5686a5874709afe21c55312
abdalla-ali <59724015+abdalla-ali@users.noreply.github.com>
2025-01-26 19:05:45 +0200

    Update backend.tf
    2

--- env/production/backend.tf
+++ env/production/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "npctfjbstate"
+    storage_account_name = "npctfjbstate2"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
