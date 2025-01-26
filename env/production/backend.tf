d534e72e01c983aa6ee0cab451179ec45a3090ee
abdalla-ali <59724015+abdalla-ali@users.noreply.github.com>
2025-01-26 19:19:02 +0200

    Delete env/production/backend.tf
    delete

--- env/production/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "RG-QCH-JB-001"
-    storage_account_name = "npctfjbstate2"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
