3893d9e6798e5260a701726b9442803aa0211ba9
abdalla-ali <59724015+abdalla-ali@users.noreply.github.com>
2025-01-26 19:08:38 +0200

    Update backend.tf
    rg add

--- env/production/backend.tf
+++ env/production/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    resource_group_name   = "RG-QCH-JB-001"
     storage_account_name = "npctfjbstate2"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
