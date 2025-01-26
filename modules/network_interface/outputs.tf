aba6c152807caa784889d17d72e703eae826c16a
abdalla-ali <59724015+abdalla-ali@users.noreply.github.com>
2025-01-26 23:56:26 +0200

    fix

--- modules/network_interface/outputs.tf
+++ modules/network_interface/outputs.tf
@@ -1,4 +1,4 @@
 output "network_interface_ids" {
   description = "List of network interface IDs"
-  value       = [for ni in azurerm_network_interface.vmr : ni.id]
+  value       = [for ni in azurerm_network_interface.nic : ni.id]
 }
